36cc75137ceb3083ecc55cd7e8f3e6df2d434269
minseongkim97 <kyrok67@naver.com>
2022-07-11 07:29:36 +0900

    Initial Commit

---
+++ week1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by MIN SEONG KIM on 2022/07/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
